60e3a059cacace40de6c3636f191dbcc99d8948d
Alexandre Bourdeaud'hui <alexandre.bourdeaudhui@gmail.com>
2017-01-03 19:29:55 +0100

    Hold Shift and Check CheckBoxes

---
+++ 10 - Hold Shift and Check Checkboxes/js/app.js
@@ -0,0 +1,53 @@
+//const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+////console.log(checkboxes);
+//
+//let lastChecked;
+//
+//function handleCheck(e) {
+//    //console.log(e);
+//    // Check if they had the shift key down
+//    // AND check that they are checking it
+//    let inBetween = false;
+//    if (e.shiftKey && this.checked) {
+//        // go ahead and do what we please
+//        // loop over every single checkboxes
+//        checkboxes.forEach(checkbox => {
+//            console.log(checkbox);
+//            if (checkbox === this || checkbox === lastChecked) {
+//                inBetween = !inBetween;
+//                console.log('Starting to check them inbetween');
+//            }
+//            
+//            if (inBetween) {
+//                checkbox.checked = true;
+//            }
+//        });
+//    }
+//    
+//    lastChecked = this;
+//}
+//checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked;
+function handleCheck(e) {
+  // Check if they had the shift key down
+  // AND check that they are checking it
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // go ahead and do what we please
+    // loop over every single checkbox
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('STarting to check them inbetween!');
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
